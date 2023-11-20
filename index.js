965da9e711ca13fa3dd5293d4f7dcdf7a5f2b74a
21951a05m7 <135637067+21951a05m7@users.noreply.github.com>
2023-11-20 11:06:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,83 +1,109 @@
 function showError(message) {
     errorContainer.textContent = "";
     errorContainer.textContent = message;
-  }
-  //  targeting elements
-  let errorContainer = document.querySelector(".error-msg");
-  let form = document.getElementById("formData");
-  let userName = document.getElementById("name");
-  let email = document.getElementById("email");
-  let password = document.getElementById("password");
-  let dob = document.getElementById("dob");
-  let checkBox = document.getElementById("agree");
-  let tableBody = document.getElementById("tableBody");
-  let btn = document.getElementById("submit");
-  //validation functions
-  function isNameEmpty(name) {
+}
+
+let errorContainer = document.querySelector(".error-msg");
+let form = document.getElementById("formData");
+let userName = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let dob = document.getElementById("dob");
+let checkBox = document.getElementById("agree");
+let tableBody = document.getElementById("tableBody");
+let btn = document.getElementById("submit");
+
+function isNameEmpty(name) {
     return name === "";
-  }
-  function isEmailEmpty(email) {
+}
+
+function isEmailEmpty(email) {
     return email === "";
-  }
-  function isPasswordEmpty(password) {
+}
+
+function isPasswordEmpty(password) {
     return password == "";
-  }
-  function isAgeEmpty(age) {
+}
+
+function isAgeEmpty(age) {
     return age == "";
-  }
-  function isInvalidAge(age) {
+}
+
+function isInvalidAge(age) {
     let currentDate = new Date();
     let userDob = new Date(age);
     let userAge = currentDate.getFullYear() - userDob.getFullYear();
     return userAge < 18 || userAge > 55;
-  }
-  //responding to submit event
-  form.addEventListener("submit", function (event) {
+}
+
+// Load stored data on page load
+document.addEventListener("DOMContentLoaded", function () {
+    let storedData = JSON.parse(localStorage.getItem("userDetailsArray")) || [];
+    storedData.forEach(function (userDetails) {
+        appendTableRow(userDetails);
+    });
+});
+
+form.addEventListener("submit", function (event) {
     event.preventDefault();
+
     let name = userName.value;
     let userEmail = email.value;
     let userPassword = password.value;
     let userDob = dob.value;
     let acceptedTerms = checkBox.checked;
+
     if (isNameEmpty(name)) {
-      showError("Name Cannot Be Empty, Please Fill That field");
-      return;
+        showError("Name Cannot Be Empty, Please Fill That field");
+        return;
     }
+
     if (isEmailEmpty(userEmail)) {
-      showError("Email is Required, Please Fill That Field");
-      return;
+        showError("Email is Required, Please Fill That Field");
+        return;
     }
+
     if (isPasswordEmpty(userPassword)) {
-      showError("Please Fill The Password");
-      return;
+        showError("Please Fill The Password");
+        return;
     }
+
     if (isAgeEmpty(userDob)) {
-      showError("Date of Birth is Required");
-      return;
+        showError("Date of Birth is Required");
+        return;
     }
+
     if (isInvalidAge(userDob)) {
-      showError("Your Age Should be Between 18 and 55");
-      return;
+        showError("Your Age Should be Between 18 and 55");
+        return;
     }
+
     showError("");
+
     let userDetails = {
-      uName: name,
-      uEmail: userEmail,
-      uPassword: userPassword,
-      uDob: userDob,
-      UTerms: acceptedTerms,
+        uName: name,
+        uEmail: userEmail,
+        uPassword: userPassword,
+        uDob: userDob,
+        UTerms: acceptedTerms,
     };
+
     let storedData = JSON.parse(localStorage.getItem("userDetailsArray")) || [];
     storedData.push(userDetails);
-    localStorage.setItem("userDetailsArray", JSON.stringify(userDetails));
     localStorage.setItem("userDetailsArray", JSON.stringify(storedData));
-  
-    tableBody.innerHTML = `<tr>
-    <td>${name}</td>
-    <td>${userEmail}</td>
-    <td>${userPassword}</td>
-    <td>${userDob}</td>
-    <td>${acceptedTerms}</td>
-  </tr>`;
+
+    // Append the new entry to the table
+    appendTableRow(userDetails);
+
     form.reset();
-  });
+});
+
+function appendTableRow(userDetails) {
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = `<td>${userDetails.uName}</td>
+                        <td>${userDetails.uEmail}</td>
+                        <td>${userDetails.uPassword}</td>
+                        <td>${userDetails.uDob}</td>
+                        <td>${userDetails.UTerms}</td>`;
+    tableBody.appendChild(newRow);
+}
